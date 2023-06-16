9df8dc10536966861de2c8b2a2b4bc108a8a05f4
IvanArt11 <artemiev1990@mail.ru>
2023-06-16 22:32:16 +0300

    Добавлена логика игры.

---
+++ components/difficulty/difficulty-component.js
@@ -0,0 +1,66 @@
+import { goToPage } from '../../index.js'
+
+export let difficulty = 0
+
+export const DifficultyPage = (app) => {
+    difficulty = 0
+    app.innerHTML = `
+  <div class="container">
+    <div class="difficulty">
+      <h2 class="difficulty__text">Выбери сложность</h2>
+      <form class="difficulty__form">
+        <div class="difficulty__value-wrap">
+          <label class="difficulty__value">
+            <input type="radio" name="difficulty" value="1">
+            <span>1</span>
+          </label>
+          <label class="difficulty__value">
+            <input type="radio" name="difficulty" value="2">
+            <span>2</span>
+          </label>
+          <label class="difficulty__value">
+            <input type="radio" name="difficulty" value="3">
+            <span>3</span>
+          </label>
+        </div>
+        <button type="submit" class="difficulty__start">Старт</button>
+      </form>
+      </div>
+  </div>
+`
+
+    // Перехватываем событие отправки формы
+    document
+        .querySelector('.difficulty__form')
+        .addEventListener('submit', (event) => {
+            // Отменяем стандартное поведение формы
+            event.preventDefault()
+
+            const radioButtons = document.querySelectorAll(
+                '.difficulty__value input[type="radio"]'
+            )
+            for (let radioButton of radioButtons) {
+                if (radioButton.checked) {
+                    difficulty = radioButton.value
+                    goToPage('Game')
+                    break
+                }
+            }
+            if (!difficulty) {
+                alert('выберите сложность')
+            }
+        })
+
+    // Рисуем видимость выбраного компонента
+    const difValues = document.querySelectorAll('.difficulty__value')
+    const startButton = document.querySelector('.difficulty__start')
+    difValues.forEach((difValue) => {
+        difValue.addEventListener('click', () => {
+            startButton.classList.add('new-game--active')
+            document
+                .querySelector('.difficulty__value--active')
+                ?.classList.remove('difficulty__value--active')
+            difValue.classList.add('difficulty__value--active')
+        })
+    })
+}
--- components/game/game.js
+++ components/game/game.js
@@ -1,111 +1,187 @@
-import { level } from '../levels-page/level-component.js'
+import { goToPage } from '../../index.js'
+import { difficulty } from '../difficulty/difficulty-component.js'
 
-// Константы для хранения значений мастей и рангов карт
-/*
-const cards = [
-    `<img class="game__card" src="./assets/images/туз пики.jpg" alt="A spades"/>`,
-    `<img class="game__card" src="./assets/images/король пики.jpg" alt="K spades"/>`,
-    `<img class="game__card" src="./assets/images/дама пики.jpg" alt="Q spades"/>`,
-    `<img class="game__card" src="./assets/images/валет пики.jpg" alt="J spades"/>`,
-    `<img class="game__card" src="./assets/images/10 пики.jpg" alt="10 spades"/>`,
-    `<img class="game__card" src="./assets/images/9 пики.jpg" alt="9 spades"/>`,
-    `<img class="game__card" src="./assets/images/8 пики.jpg" alt="8 spades"/>`,
-    `<img class="game__card" src="./assets/images/7 пики.jpg" alt="7 spades"/>`,
-    `<img class="game__card" src="./assets/images/6 пики.jpg" alt="6 spades"/>`,
-
-    `<img class="game__card" src="./assets/images/туз черви.jpg" alt="A hearts"/>`,
-    `<img class="game__card" src="./assets/images/король черви.jpg" alt="K hearts"/>`,
-    `<img class="game__card" src="./assets/images/дама черви.jpg" alt="Q hearts"/>`,
-    `<img class="game__card" src="./assets/images/валет черви.jpg" alt="J hearts"/>`,
-    `<img class="game__card" src="./assets/images/10 черви.jpg" alt="10 hearts"/>`,
-    `<img class="game__card" src="./assets/images/9 черви.jpg" alt="9 hearts"/>`,
-    `<img class="game__card" src="./assets/images/8 черви.jpg" alt="8 hearts"/>`,
-    `<img class="game__card" src="./assets/images/7 черви.jpg" alt="7 hearts"/>`,
-    `<img class="game__card" src="./assets/images/6 черви.jpg" alt="6 hearts"/>`,
-
-    `<img class="game__card" src="./assets/images/туз бубны.jpg" alt="A diamonds"/>`,
-    `<img class="game__card" src="./assets/images/король бубны.jpg" alt="K diamonds"/>`,
-    `<img class="game__card" src="./assets/images/дама бубны.jpg" alt="Q diamonds"/>`,
-    `<img class="game__card" src="./assets/images/валет бубны.jpg" alt="J diamonds"/>`,
-    `<img class="game__card" src="./assets/images/10 бубны.jpg" alt="10 diamonds"/>`,
-    `<img class="game__card" src="./assets/images/9 бубны.jpg" alt="9 diamonds"/>`,
-    `<img class="game__card" src="./assets/images/8 бубны.jpg" alt="8 diamonds"/>`,
-    `<img class="game__card" src="./assets/images/7 бубны.jpg" alt="7 diamonds"/>`,
-    `<img class="game__card" src="./assets/images/6 бубны.jpg" alt="6 diamonds"/>`,
-
-    `<img class="game__card" src="./assets/images/туз крести.jpg" alt="A clubs"/>`,
-    `<img class="game__card" src="./assets/images/король крести.jpg" alt="K clubs"/>`,
-    `<img class="game__card" src="./assets/images/дама крести.jpg" alt="Q clubs"/>`,
-    `<img class="game__card" src="./assets/images/валет крести.jpg" alt="J clubs"/>`,
-    `<img class="game__card" src="./assets/images/10 крести.jpg" alt="10 clubs"/>`,
-    `<img class="game__card" src="./assets/images/9 крести.jpg" alt="9 clubs"/>`,
-    `<img class="game__card" src="./assets/images/8 крести.jpg" alt="8 clubs"/>`,
-    `<img class="game__card" src="./assets/images/7 крести.jpg" alt="7 clubs"/>`,
-    `<img class="game__card" src="./assets/images/6 крести.jpg" alt="6 clubs"/>`,
-];
-*/
-// const suits = ['черви', 'бубны', 'крести', 'пики'];
-// const ranks = ['6', '7', '8', '9', '10', 'Q', 'K', 'J', 'A'];
-
-let cardsArr = [] // Массив для хранения сгенерированных карт
-/*
-let selectedDifficulty = '1' // Уровень сложности по умолчанию
-let flippedCards = [] // Массив для хранения перевернутых карт
-let matchedCards = [] // Массив для хранения совпавших карт
-let gameStarted = false // Флаг для проверки начала игры
-let startTime // Время начала игры
-*/
-
-// Функция для перемешивания массива
-/*
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+let arrCards = []
+const getSuit = (num) => {
+    switch (num) {
+        case 1:
+            return 'spades'
+        case 2:
+            return 'hearts'
+        case 3:
+            return 'diamonds'
+        case 4:
+            return 'clubs'
+        default:
+            return 'error'
     }
 }
-*/
-
-// Функция для генерации карт
-/*
-function generateCards(difficulty) {
-    cards = []
-    const totalPairs = difficulty === "1" ? 6 : difficulty === "2" ? 12 : 18
-    const selectedCards = suits.slice(0, totalPairs)
-    // const selectedSuits = suits.slice(0, totalPairs);
-    // const selectedRanks = ranks.slice(0, totalPairs);
-
-    for (let i = 0; i < totalPairs; i++) {
-        const card1 = { cards: selectedCards[i] }
-        const card2 = { cards: selectedCards[i] }
-        // const card1 = { suit: selectedSuits[i], rank: selectedRanks[i] };
-        // const card2 = { suit: selectedSuits[i], rank: selectedRanks[i] };
-        cards.push(card1, card2)
+const getRank = (num) => {
+    switch (num) {
+        case 6:
+            return 'six'
+        case 7:
+            return 'seven'
+        case 8:
+            return 'eight'
+        case 9:
+            return 'nine'
+        case 10:
+            return 'ten'
+        case 11:
+            return 'jack'
+        case 12:
+            return 'queen'
+        case 13:
+            return 'king'
+        case 14:
+            return 'ace'
+        default:
+            return 'error'
     }
-
-    shuffleArray(cards)
 }
-*/
+const checkTheWin = () => {
+    const cards = document.querySelectorAll('.game__card')
+    for (let card of cards) {
+        if (card.dataset.status !== 'open') {
+            return false
+        }
+    }
+    return true
+}
+const checkTheCuple = (firstOpenCard, secondOpenCard) => {
+    if (firstOpenCard.value !== secondOpenCard.value) {
+        alert('Вы проиграли')
+        goToPage('Difficulty')
+    } else {
+        firstOpenCard.nod.setAttribute('data-status', 'open')
+        secondOpenCard.nod.setAttribute('data-status', 'open')
 
-const cardsArray = (numbers) => {
-    for (let i = 0; i < numbers; i++) {
-        cardsArr.push(
-            `<img class="game__card" src="./assets/images/рубашка.jpg" alt="card"/>`
+        if (checkTheWin()) {
+            alert('Вы победили')
+            goToPage('Difficulty')
+        } else {
+            firstOpenCard.value = ''
+            firstOpenCard.nod = null
+
+            secondOpenCard.value = ''
+            secondOpenCard.nod = null
+        }
+    }
+}
+
+const startGame = () => {
+    let firstOpenCard = {
+        value: '',
+        nod: null,
+    }
+    let secondOpenCard = {
+        value: '',
+        nod: null,
+    }
+    // актвируем кнопку Начать заново
+    const newGame = document.querySelector('.new-game')
+    newGame.classList.add('new-game--active')
+    newGame.addEventListener('click', () => {
+        goToPage('Difficulty')
+    })
+
+    // запуск таймера
+    const min = document.querySelector('.timer__counter--min')
+    const sec = document.querySelector('.timer__counter--sec')
+
+    let second = ''
+    let minute = ''
+    setInterval(() => {
+        second = Number(sec.innerHTML) + 1
+        if (second < 60) {
+            sec.innerHTML = Number(second) < 10 ? '0' + second : second
+        } else {
+            minute = Number(min.innerHTML) + 1
+            min.innerHTML = Number(minute) < 10 ? '0' + minute : minute
+            sec.innerHTML = '00'
+        }
+    }, 1000)
+
+    // Ивент на клик карты
+    const cards = document.querySelectorAll('.game__card')
+    cards.forEach((card) => {
+        card.addEventListener('click', () => {
+            // проверка, чтобы ивент не отрабатывал на уже открытые карты
+            if (card.dataset.status !== 'open') {
+                card.setAttribute(
+                    'src',
+                    `../../assets/images/${card.dataset.value}.jpg`
+                )
+                // если первая карта открыта переворачиваем вторую и проверяем пара ли это
+                if (!firstOpenCard.value) {
+                    firstOpenCard.value = card.dataset.value
+                    firstOpenCard.nod = card
+                } else {
+                    secondOpenCard.value = card.dataset.value
+                    secondOpenCard.nod = card
+                    checkTheCuple(firstOpenCard, secondOpenCard)
+                }
+            }
+        })
+    })
+}
+const fillCardsArray = (numbers) => {
+    // генерация карт
+    let rank = ''
+    let suit = ''
+    for (let i = 0; i < numbers / 2; i++) {
+        // проверка на больше одной пары одинаковых карт в игре
+        do {
+            rank = getRank(Math.floor(Math.random() * 9) + 6)
+            suit = getSuit(Math.floor(Math.random() * 4) + 1)
+        } while (
+            arrCards.includes(
+                `<img data-value="${rank}-of-${suit}" class="game__card" src="../../assets/images/${rank}-of-${suit}.jpg" alt="card"/>`
+            )
         )
+
+        let card = `<img data-value="${rank}-of-${suit}" class="game__card" src="../../assets/images/${rank}-of-${suit}.jpg" alt="card"/>`
+        arrCards.push(card)
+
+        // добавляем пару
+        let index = Math.floor(Math.random() * numbers) + 1
+
+        // поиск случайного места для пары
+        let isFindSlot = false
+        while (!isFindSlot) {
+            if (!arrCards[index]) {
+                arrCards[index] = card
+                isFindSlot = true
+            } else if (index === numbers) {
+                index = 1
+            } else {
+                index++
+            }
+        }
     }
+
+    // переворачиваем карты, рубашкой вверх
+    setTimeout(() => {
+        const cards = document.querySelectorAll('.game__card')
+        cards.forEach((card) => {
+            card.setAttribute('src', '../../assets/images/dealer-shirt.jpg')
+        })
+    }, 5000)
 }
 
 export const Game = (app) => {
     // опции сложности
-    switch (level) {
+    arrCards = []
+    switch (difficulty) {
         case '1':
-            cardsArray(6)
+            fillCardsArray(6)
             break
         case '2':
-            cardsArray(12)
+            fillCardsArray(12)
             break
         case '3':
-            cardsArray(18)
+            fillCardsArray(18)
             break
         default:
             break
@@ -113,23 +189,26 @@ export const Game = (app) => {
 
     app.innerHTML = `
     <div class="container">
-      <div class="game">
-        <div class="header">
-            <div class="timer">
-                <div class="timer__text">
-                    <div class="timer__text-min">min</div>
-                    <div class="timer__text-sec">sec</div>
+        <div class="game">
+            <div class="header">
+                <div class="timer">
+                    <div class="timer__text">
+                        <div class="timer__text-min">min</div>
+                        <div class="timer__text-sec">sec</div>
+                    </div>
+                    <div class="timer__counter">
+                    <span class="timer__counter--min">00</span>.<span class="timer__counter--sec">00</span></div>
                 </div>
-                <div class="timer__counter">00.00</div>
+                <button class="new-game">Начать заново</button>
             </div>
-            <button class="new-game">Начать заново</button>
+            <main class="game-wrap">
+                <div class="game__field">
+                    ${arrCards.map((card) => card).join('')}
+                </div>
+            </main>
         </div>
-        <main class="game-wrap">
-            <div class="game__field">
-                ${cardsArr.map((card) => card).join('')}
-            </div>
-        </main>
-      </div>
     </div>
   `
+    // Запуск игры, активность кнопок и запуск таймера после предпоказа карт
+    setTimeout(startGame, 5000)
 }
