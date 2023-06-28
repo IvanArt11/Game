e08f0a87159b35e6fdc9f4f2fdbf51cfb4fadc65
IvanArt11 <artemiev1990@mail.ru>
2023-06-28 23:02:27 +0300

    .

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,11 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
+    transform: {
+        '^.+\\.(ts|tsx)?$': 'ts-jest',
+        '^.+\\.(js|jsx)$': 'babel-jest',
+    },
     moduleNameMapper: {
         '\\.(css|less|sass|scss)$': '<rootDir>/jest/stylesTransform.js',
     },
-    //setupFilesAfterEnv: ['<rootDir>/setup-tests.js'],
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
-const isProduction = process.env.NODE_ENV === 'production'
+const isProduction = process.env.NODE_ENV === 'development'
 module.exports = {
     entry: './index.ts',
     mode: isProduction ? 'production' : 'development',
